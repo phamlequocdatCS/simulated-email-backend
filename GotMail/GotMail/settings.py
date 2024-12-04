7251f523526021accfb54708953894b03ebc2e64
phamlequocdatCS <phamlequocdat2017@gmail.com>
2024-12-04 23:21:45 +0700

    try allow all

--- GotMail/GotMail/settings.py
+++ GotMail/GotMail/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
-
+CORS_ALLOW_ALL_ORIGINS = True
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
 else:
